4c47e1e86c67652d6dd92bbb2dd2f6f3e8543d56
itsuki <itsuki.enjyoy@gmail.com>
2024-11-24 12:53:35 +0900

    Initial Commit

---
+++ CustomLayoutDemo/CustomLayoutDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomLayoutDemoApp.swift
+//  CustomLayoutDemo
+//
+//  Created by Itsuki on 2024/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomLayoutDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
